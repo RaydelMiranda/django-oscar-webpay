544aa5facec3bf2b54bfa2665ec70ebd1900dc71
Raydel Miranda <raydel.miranda.gomez@gmail.com>
2017-05-11 23:14:24 -0400

    Staging things

---
+++ oscar_webpay/exceptions/common.py
@@ -0,0 +1,42 @@
+from django.utils.translation import ugettext_lazy as _
+
+
+class MissingShippingMethodException(Exception):
+
+    def __init__(self, value):
+        self.message = _(u"Shipping method expected, reveiced: {}".format(value))
+
+
+class MissingShippingAddressException(Exception):
+
+    def __init__(self, value):
+        self.message = _(u"Shipping address expected, recived: {}".format(value))
+
+
+class TransactionException(Exception):
+
+    def __init__(self, *args, **kwargs):
+        super(TransactionException, *args, **kwargs)
+
+
+class AuthenticationError(TransactionException):
+    pass
+
+
+class FailedTransaction(TransactionException):
+
+    def __init__(self, cause=None, *args, **kwargs):
+        self.message = _(u'Failed transaction motive: {}'.format(cause if cause else _(u'Unknown')))
+        super(FailedTransaction, self).__init__(*args, **kwargs)
+
+
+class TimeLimitExceeded(TransactionException):
+    pass
+
+
+class AbortedTransactionByCardHolder(TransactionException):
+    pass
+
+class U3Exception(TransactionException):
+    """ Internal error. """
+    pass
