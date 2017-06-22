b84c3a21899842d832a6690f69fc0aad7ec454e6
Raydel Miranda <raydel.miranda.gomez@gmail.com>
2017-06-22 10:01:52 -0400

    Repairing bug, syntax error.

--- oscar_webpay/exceptions/soap_exceptions.py
+++ oscar_webpay/exceptions/soap_exceptions.py
@@ -1,5 +1,4 @@
 
-
 from django.utils.translation import ugettext_lazy as _
 
 
@@ -8,49 +7,61 @@ class CVVError(Exception):
         self.message = _(u'CVV Error, please check and try again')
         super(CVVError, self).__init__()
 
+
 class CARDError(Exception):
     def __init__(self):
         self.message = _(u'Card Error, please check and try again')
+
 
 class OLDCARDError(Exception):
     def __init__(self):
         self.message = _(u'Your card has expired')
 
+
 class ERR_TBK_CARD(Exception):
     def __init__(self):
         self.message = _(u'Wrong card type, please check and try again')
+
 
 class ERR_TBK_NUMBER_QUOTAS(Exception):
     def __init__(self):
         self.message = _(u'Quota error, wrong number of cuotes, please check and try again')
 
+
 class ERR_TBK_NUMBER_CARD(Exception):
     def __init__(self):
         self.message = _(u'Card Error, wrong number, please check and try again')
+
 
 class ERR_TBK_CVV(Exception):
     def __init__(self):
         self.message = _(u'CVV Error, please check and try again')
 
+
 class ERR_TIMEOUT(Exception):
     def __init__(self):
         self.message = _(u'Time limit exedeed, please check and try again')
+
 
 class ERR_RUT(Exception):
     def __init__(self):
         self.message = _(u'Rut Error, please check and try again')
 
+
 class ERR_CCV(Exception):
     def __init__(self):
         self.message = _(u'Security code error')
+
 
 class ERR_EXPIRATION_DATE(Exception):
     def __init__(self):
         self.message = _(u'Error in the expiration date')
 
+
 class ERR_AMOUNT(Exception):
     def __init__(self):
         self.message = _(u'Amount error')
+
 
 class SOAPException(Exception):
 
@@ -70,10 +81,10 @@ class SOAPException(Exception):
     def __init__(self, error_code):
 
         self.__generic_msg = \
-            _("The possible causes of this rejection are:") \
-            _("- Failure to enter the data of your Credit or Debit card (date and / or security code).")\
-            _("- Your credit or debit card does not have enough balance.") \
-            _("- Card not yet enabled in the financial system.")
+            _("The possible causes of this rejection are:"
+              "- Failure to enter the data of your Credit or Debit card (date and / or security code)."
+              "- Your credit or debit card does not have enough balance."
+              "- Card not yet enabled in the financial system")
 
         self.message = self.__generic_msg
         self.__error_code = error_code
